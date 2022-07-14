eb8553ffab3098551db6942e62df65f94445d331
jokerav <jokeravdeveloper@gmail.com>
2022-07-14 20:27:55 +0300

    Update store.js

--- src/contactsAPI/store.js
+++ src/contactsAPI/store.js
@@ -1,9 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { contactsApi } from './contactsAPI';
-// import contactsReducer from './contacts-reducer';
 import filterSlice from '../contactsAPI/filterSlice';
-// import {changeFilter} from '../contactsAPI/filterSlice';
-
 export const store = configureStore({
   reducer: {
     [contactsApi.reducerPath]: contactsApi.reducer,

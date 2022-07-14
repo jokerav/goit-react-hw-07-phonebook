b787841d34ac7ad7db2addfd78b0f4ce0c86dba0
jokerav <jokeravdeveloper@gmail.com>
2022-07-14 16:59:59 +0300

    123

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,30 +1,29 @@
 import Contact from '../Contact/Contact';
 import { useSelector } from 'react-redux';
 import { getFilter } from '../../contactsAPI/selectors';
-
 import { useGetContactsQuery } from '../../contactsAPI/contactsAPI';
 
 const ContactList = () => {
-  const { data: contacts } = useGetContactsQuery();
   const filter = useSelector(getFilter);
+  const { data: contacts, error, isLoading } = useGetContactsQuery();
   console.log(filter, contacts);
 
-  const getVisibleContacts = () => {
-    const normFilter = filter.toLowerCase();
-    return contacts.filter(({ name }) =>
-      name.toLowerCase().includes(normFilter)
-    );
-  };
+  // const getVisibleContacts = () => {
+  //   const normFilter = filter.toLowerCase();
+  //   return contacts.filter(({ name }) =>
+  //     name.toLowerCase().includes(normFilter)
+  //   );
+  // };
 
-  const visibleContacts = getVisibleContacts();
-  console.log(visibleContacts);
+  // const visibleContacts = getVisibleContacts();
+  // console.log(visibleContacts);
 
   return (
     <ul>
-      {visibleContacts.length > 0 &&
-        visibleContacts.map(contact => (
-          <Contact key={contact.id} contact={contact} />
-        ))}
+      {isLoading && <p>Грузим...</p>}
+      {error && <p>Ошибочка...</p>}
+      {contacts &&
+        contacts.map(contact => <Contact key={contact.id} contact={contact} />)}
     </ul>
   );
 };
--- src/contactsAPI/store.js
+++ src/contactsAPI/store.js
@@ -1,16 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { contactsApi } from './contactsAPI';
 // import contactsReducer from './contacts-reducer';
-import  filterSlice from '../contactsAPI/filterSlice'
+import filterSlice from '../contactsAPI/filterSlice';
 // import {changeFilter} from '../contactsAPI/filterSlice';
-
 
 export const store = configureStore({
   reducer: {
     [contactsApi.reducerPath]: contactsApi.reducer,
-    filter :filterSlice,
-
-
+    filter: filterSlice,
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware().concat(contactsApi.middleware),

19590e79a7078e30aa6c9f370e75c07475d90e6c
jokerav <jokeravdeveloper@gmail.com>
2022-07-14 18:30:39 +0300

    фильтр работает!

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,28 +2,29 @@ import Contact from '../Contact/Contact';
 import { useSelector } from 'react-redux';
 import { getFilter } from '../../contactsAPI/selectors';
 import { useGetContactsQuery } from '../../contactsAPI/contactsAPI';
-
 const ContactList = () => {
   const filter = useSelector(getFilter);
-  const { data: contacts, error, isLoading } = useGetContactsQuery();
+  const { data: contacts = [], error, isLoading } = useGetContactsQuery();
   console.log(filter, contacts);
 
-  // const getVisibleContacts = () => {
-  //   const normFilter = filter.toLowerCase();
-  //   return contacts.filter(({ name }) =>
-  //     name.toLowerCase().includes(normFilter)
-  //   );
-  // };
+  const getVisibleContacts = () => {
+    const normFilter = filter.toLowerCase();
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(normFilter)
+    );
+  };
 
-  // const visibleContacts = getVisibleContacts();
-  // console.log(visibleContacts);
+  const visibleContacts = getVisibleContacts();
+  console.log(visibleContacts);
 
   return (
     <ul>
       {isLoading && <p>Грузим...</p>}
       {error && <p>Ошибочка...</p>}
-      {contacts &&
-        contacts.map(contact => <Contact key={contact.id} contact={contact} />)}
+      {visibleContacts.length > 0 &&
+        visibleContacts.map(contact => (
+          <Contact key={contact.id} contact={contact} />
+        ))}
     </ul>
   );
 };
--- src/contactsAPI/contactsAPI.jsx
+++ src/contactsAPI/contactsAPI.jsx
@@ -31,6 +31,7 @@ export const contactsApi = createApi({
     }),
   }),
 });
+
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
 export const {

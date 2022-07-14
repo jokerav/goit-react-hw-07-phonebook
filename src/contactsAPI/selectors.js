c20c4287e0cc28adf49ab4c76899baf72a132c71
jokerav <jokeravdeveloper@gmail.com>
2022-07-14 20:28:14 +0300

    Update selectors.js

--- src/contactsAPI/selectors.js
+++ src/contactsAPI/selectors.js
@@ -1,12 +1,1 @@
 export const getFilter = state => state.filter;
-
-// const getContacts = state =>
-//   state.contactsAPI.queries.getContacts(undefined).data;
-
-// const getVisibleContacts = state => {
-//   const filter = getfilter(state);
-//   const contacts = getContacts(state);
-//   const normFilter = filter.toLowerCase();
-//   return contacts.filter(({ name }) => name.toLowerCase().includes(normFilter));
-// };
-// export default { getFilter };
